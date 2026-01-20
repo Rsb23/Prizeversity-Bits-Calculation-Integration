245332e73b8765a1014fdc1250d02dbe39450814
Rsb23 <rsbarbu23@gmail.com>
2026-01-19 21:00:09 -0500

    feat: url routing at app level for downloading and removing files from uploaded files list

--- pv_extras/bit_calculator/urls.py
+++ pv_extras/bit_calculator/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('csv/', views.csv, name='csv'),
     path('about/', views.about, name='about'),
     path('faq/', views.faq, name='faq'),
+    path('download-uploaded-file/', views.download_uploaded_file, name='download_uploaded_file'),
+    path('remove-uploaded-file/', views.remove_uploaded_file, name='remove_uploaded_file'),
 ]
